8b57bc5a063a7e61a764c12deb82a26e7fdc6dbb
nathanmoore21 <n00201743@iadt.ie>
2022-04-09 14:10:20 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
+                <h1> HELOOOO</h1>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
